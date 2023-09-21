faf5c8134abb4d96f0a56821c808503937e29e9a
Miroslaw Zielinski <mirek@parasoft.com>
2023-09-21 10:38:14 +0000

    Test

--- mainTestSuite.cxx
+++ mainTestSuite.cxx
@@ -12,22 +12,22 @@ void teardown()
 }
 };
 
-TEST(main, main_1)
+TEST(main, main_1_reqCIC_4)
 {
     CHECK_EQUAL(getValue(2,2), 4);
 }
 
-TEST(main, main_2)
+TEST(main, main_2_reqCIC_reqCIC_5)
 {
     CHECK_EQUAL(getValue(3,2), 6);
 }
 
-TEST(main, main_3)
+TEST(main, main_3_reqCIC_5)
 {
     CHECK_EQUAL(getValue(1,1), 1);
 }
 
-TEST(main, main_4)
+TEST(main, main_4_reqCIC_6)
 {
     CHECK_EQUAL(getValue(-1,1), 14);
 }
