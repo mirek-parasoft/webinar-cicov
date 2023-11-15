98c0f38d9fb29d152eb1ba4d770c709f38a345b2
Miroslaw Zielinski <mirek@parasoft.com>
2023-11-15 08:22:03 +0000

    Improving coverage

--- include/sensorUtil.hxx
+++ include/sensorUtil.hxx
@@ -1,1 +1,7 @@
-int getAvarage(int *, int);
+#include <cstdint>
+
+int32_t getAvarage(int32_t *, int32_t);
+
+
+
+
--- sensorUtil.cxx
+++ sensorUtil.cxx
@@ -1,15 +1,15 @@
 #include "include/sensorUtil.hxx"
 
-int getAvarage(int * buf, int count) {
+int32_t getAvarage(int32_t * buf, int32_t count) {
     int res = 0;
 
     if (buf == 0 || count == 0) {
         return 0;
     }
 
-    for (int i = 0; i < count; i++) {
+    for (uint16_t i = 0; i < count; i++) {
         res += buf[i];
     }
 
-    return (int) res/count;
+    return (int32_t) res/count;
 } 
