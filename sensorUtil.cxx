d98604a0dea38a7efdfcff1ee976ed1ace1a913c
Miroslaw Zielinski <mirek@parasoft.com>
2023-11-16 07:05:13 +0000

    Demo reverted

--- sensorUtil.cxx
+++ sensorUtil.cxx
@@ -1,10 +1,10 @@
 #include "include/sensorUtil.hxx"
 
-
+/*
 int32_t getAvarage(int32_t * buf, int32_t count) {
     int res = 0;
 
-    if (buf == 0 || count == 0) {
+    if (buf == 0) {
         return 0;
     }
 
@@ -14,3 +14,4 @@ int32_t getAvarage(int32_t * buf, int32_t count) {
 
     return (int32_t) res/count;
 } 
+*/
